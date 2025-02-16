1ad3cc79ec6aa3ef8e192f5a8e8435d5d7a3c00f
Syed Ukkashah <115150510+syedukkashah@users.noreply.github.com>
2025-02-16 19:04:46 +0500

    Update Task9.asm

--- Labs/03/Task 9/Task9.asm
+++ Labs/03/Task 9/Task9.asm
@@ -1,1 +1,15 @@
+TITLE Repeat "TEST" 500 Times
+INCLUDE Irvine32.inc
+
+.data
+testString BYTE 500 dup("TEST"), 0  
+
+.code
+main PROC
+    mov edx, OFFSET testString  
+    call WriteString           
+    call Crlf                 
+    exit
+main ENDP
+END main
 
