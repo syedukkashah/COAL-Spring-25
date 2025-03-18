2bbd284ff86b352426b6b2273d300467968b1d8e
Syed Ukkashah <115150510+syedukkashah@users.noreply.github.com>
2025-03-19 02:11:56 +0500

    Update Q1.asm

--- Labs/03/Q1.asm
+++ Labs/03/Q1.asm
@@ -1,3 +1,9 @@
 .data
 val1  SWORD ?    ; 16-bit signed integer (uninitialized)
 val2  SBYTE -11  ; 8-bit signed integer (initialized to -11)
+.code
+main proc
+	; nothing here
+exit
+main endp
+end main
