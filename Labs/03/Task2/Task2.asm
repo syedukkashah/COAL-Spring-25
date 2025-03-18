e9fffa17ea6cb2a9d6d37dda652b3fe5de1e35c6
Syed Ukkashah <115150510+syedukkashah@users.noreply.github.com>
2025-03-19 00:42:51 +0500

    Update Task2.asm

--- Labs/03/Task2/Task2.asm
+++ Labs/03/Task2/Task2.asm
@@ -1,15 +1,12 @@
-TITLE Smallest 2-bit Signed Integer
 INCLUDE Irvine32.inc
-
 .data
-val3  SBYTE -2  
-
+	val3 sdword ?
 .code
 main PROC
-    movsx ax, val3  
-
-    call DumpRegs   
-
-    exit
+	mov eax, 1 
+	shl eax, 31   ;shifting to the left by 31 bits
+	mov val3, eax
+	call DumpRegs
+exit
 main ENDP
 END main
